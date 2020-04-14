47d3a83dc86c756419da2a7495469fea548cafab
Julia Benz <50639046+jbenz7@users.noreply.github.com>
2020-04-14 16:27:31 -0500

    Graphs are loading and switching, geometry is a little funky, working on that

--- gui/mainWindow.py
+++ gui/mainWindow.py
@@ -1,6 +1,7 @@
 import os
 import logging
 import numpy as np
+import imageio
 
 import yaml
 from PyQt5.QtCore import *
@@ -10,12 +11,14 @@ from PyQt5.QtGui import *
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as figureCanvas
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as navigationToolbar
 from matplotlib.figure import Figure
-from matplotlib.pyplot import *
+import matplotlib.pyplot as plt
 
 from util.fileDialog import FileDialog
 from generated import mainWindow_ui
 from pathlib import Path
+from util import constants, util
 import random
+
 logger = logging.getLogger(__name__)
 
 
@@ -25,6 +28,10 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_BrewMaster):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
+        self.plotWidget.mpl_connect('motion_notify_event', self.on_plotWidget_mouseMoved)
+        self.plotWidget.mpl_connect('key_press_event', self.on_plotWidget_keyPressed)
+        self.plotWidget.mpl_connect('button_press_event', self.on_plotWidget_clicked)
+
         self.setWindowTitle("BrewMaster Homebrewing Assistant")
         self.setWindowIcon(QIcon('beer_detail.png'))
 
@@ -33,7 +40,18 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_BrewMaster):
         self.pHList = []
 
 
+    @pyqtSlot()
+    def on_plotWidget_mouseMoved(self, event):
+        pass
+        # print('on_sliceWidgetBMode_mouseMoved')
 
+    @pyqtSlot()
+    def on_plotWidget_keyPressed(self, event):
+        pass
+
+    @pyqtSlot()
+    def on_plotWidget_clicked(self, event):
+        pass
 
     @pyqtSlot()
     def on_searchForData_clicked(self):
@@ -52,6 +70,7 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_BrewMaster):
         headers = dataList[0]
         for i in range(1, len(dataList)):
 
+            #extracts time, temperature, and pH data from the text file and categorizes the information into their own lists
             currentData = dataList[i].split(',')
 
             time = float(currentData[0].strip())
@@ -63,6 +82,35 @@ class MainWindow(QMainWindow, mainWindow_ui.Ui_BrewMaster):
             pH = float(currentData[2].strip())
             self.pHList.append(pH)
 
+    @pyqtSlot(bool)
+    def on_temperatureButton_toggled(self, checked):
+        print("temperature box checked")
+        if self.temperatureButton.isChecked() == False:
+            self.plotWidget.clearFigure()
 
-            tempPlot = plot(self.timeList[i], self.tempList[i])
-            self.graphWidget.plot = tempPlot
+        if self.temperatureButton.isChecked():
+            self.plotWidget.plotTemperature(self.timeList, self.tempList)
+        if self.pHButton.isChecked():
+            self.plotWidget.plotpH(self.timeList, self.pHList)
+
+        if self.pHButton.isChecked() and self.temperatureButton.isChecked():
+            self.plotWidget.clearFigure()
+            self.plotWidget.plotBoth(self.timeList, self.tempList, self.pHList)
+
+    @pyqtSlot(bool)
+    def on_pHButton_toggled(self, checked):
+        print('pH box checked')
+        if self.pHButton.isChecked() == False:
+            self.plotWidget.clearFigure()
+
+        if self.pHButton.isChecked():
+            self.plotWidget.plotpH(self.timeList, self.pHList)
+        if self.temperatureButton.isChecked():
+            self.plotWidget.plotTemperature(self.timeList, self.tempList)
+
+
+        if self.temperatureButton.isChecked() and self.pHButton.isChecked():
+            self.plotWidget.clearFigure()
+            self.plotWidget.plotBoth(self.timeList, self.tempList, self.pHList)
+
+
---
+++ gui/sliceWidget.py
@@ -0,0 +1,69 @@
+import matplotlib
+import numpy as np
+
+matplotlib.use('Qt5Agg')
+from PyQt5 import QtWidgets
+
+from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
+from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
+from matplotlib.figure import Figure
+import matplotlib.patches as patches
+from PyQt5.Qt import *
+import matplotlib.pyplot as plt
+
+import cv2
+import imageio
+
+class SliceWidget(FigureCanvas):
+    def __init__(self, parent=None, dpi=100):
+        # Create figure and axes, the axes should cover the entire figure size
+        self.figure = Figure(dpi=dpi, frameon=False)
+        self.axes = self.figure.add_axes((1/12, 1/12, 11/12, 11/12), facecolor='white')
+
+        # Show the x and y axis
+        self.axes.set_axis_on()
+
+
+        # Initialize the parent FigureCanvas
+        FigureCanvas.__init__(self, self.figure)
+        self.setParent(parent)
+
+        # Set widget to have strong focus to receive key press events
+        self.setFocusPolicy(Qt.StrongFocus)
+
+        # Create navigation toolbar and hide it
+        # We don't want the user to see the toolbar but we are making our own in the user interface that will call
+        # functions from the toolbar
+        self.toolbar = NavigationToolbar(self, self)
+        self.toolbar.hide()
+
+        # Update size policy and geometry
+        FigureCanvas.setSizePolicy(self, QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Expanding)
+        FigureCanvas.updateGeometry(self)
+
+    def plotTemperature(self, time, temp):
+
+        #plots the temperature versus time
+        self.axes.plot(time, temp, label='Temperature', color='red')
+        self.draw()
+
+    def plotpH(self, time, pH):
+
+        #plots the pH versus time
+        self.axes.plot(time, pH, label='pH', color='blue')
+        self.draw()
+
+    def plotBoth(self, time, temp, pH):
+
+        self.figure, self.axes = plt.subplots(2,1)
+        self.axes[0].plot(time, temp, color='red')
+        self.axes[0].set_title('Temperature')
+        self.axes[1].plot(time, pH, color='blue')
+        self.axes[1].set_title('pH')
+
+        self.draw()
+
+    def clearFigure(self):
+        self.axes = self.figure.add_axes((1 / 12, 1 / 12, 11 / 12, 11 / 12), facecolor='white')
+        self.axes.clear()
+        self.draw()
